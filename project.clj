b5462697fa3d38ec3e48ca6e4afce8104e8ca369
wiseman <jjwiseman@gmail.com>
2015-06-22 14:39:21 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.lemonodor/syllables "0.1.2-SNAPSHOT"
+  :description "Clojure library for counting syllables in words."
+  :url "https://github.com/wiseman/clj-syllables"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :deploy-repositories [["releases" :clojars]]
+  :dependencies [[org.clojure/clojure "1.6.0"]])
