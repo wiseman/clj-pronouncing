546f9268316344324579bbc8237524efebcb454b
wiseman <jjwiseman@gmail.com>
2015-06-23 17:47:47 -0700

    Version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemonodor/pronouncing "0.0.4-SNAPSHOT"
+(defproject com.lemonodor/pronouncing "0.0.4"
   :description "Clojure interface to the CMU pronouncing dictionary. Provides ways to look up word pronounciations, count syllables, and find rhyming words."
   :url "https://github.com/wiseman/clj-pronouncing"
   :license {:name "MIT License"
